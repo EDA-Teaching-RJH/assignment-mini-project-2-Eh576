855ca138321bb1c97bc75fe31e2438e202c23bbc
Eh576 <Eh576@kent.ac.uk>
2026-03-26 23:43:25 +0000

    Basic Functions (mainly from L7)

--- EH576.py
+++ EH576.py
@@ -1,3 +1,19 @@
 import re # import to check email format
 import csv # import to save ratings in cvs file
 from pathlib import Path # import path to check if file has already existed 
+
+EMAIL_PATTERN = r"^[\w\.-]+@[\w\.-]+\.\w+$" # regex patern for a basic valid email 
+
+def clean_name(name):
+    return " ".join(part.capitalize() for part in name.strip().split()) # clean name
+
+def valid_email_cheker(email):
+    return re.match(EMAIL_PATTERN, email.strip()) is not None #check if email matches patern
+
+def main():
+    print(clean_name("  alice smith "))  # test name cleaning
+    print(valid_email_cheker("alice@example.com"))  # test email validation
+
+
+if __name__ == "__main__":
+    main()
