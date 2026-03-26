dc572364f9bcd8ade9db15b509f41d620f0a7e92
Eh576 <Eh576@kent.ac.uk>
2026-03-26 18:50:02 +0000

    Importing all the libraries

--- EH576.py
+++ EH576.py
@@ -1,1 +1,3 @@
-print("Hello world 2")
+import re # import to check email format
+import csv # import to save ratings in cvs file
+from pathlib import Path # import path to check if file has already existed 
